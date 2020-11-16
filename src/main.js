54ac18631165e8bc48fbcb2904cc07405b280425
96197 <961977096@qq.com>
2020-11-16 21:12:23 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
+import './public-path';
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-Vue.config.productionTip = false
+let instance = null;
+function render(props) {
+  instance=new Vue({
+    router,
+    render:h=>h(App)
+  }).$mount('#app');
+}
 
-new Vue({
-  router,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+if (!window.__POWERED_BY_QIANKUN__) {
+  render();
+}
+
+export async function bootstrap() {
+  console.log('%c ', 'color: green;', 'vue3.0 app bootstraped');
+}
+
+export async function mount(props) {
+  render(props)
+}
+
+export async function unmount() {
+  instance.$destroy()
+}
